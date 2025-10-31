efee128a477b21f8fc6e83c963cd5243de5744a3
RadimYassin <radimyassin20@gmail.com>
2025-10-31 20:46:37 +0100

    TP 9 : Client React pour API REST

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CompteList from './components/CompteList';
+import CompteForm from './components/CompteForm';
+
+function App() {
+  return (
+    <div className="App">
+      <CompteForm />
+      <CompteList />
+    </div>
+  );
+}
+
+export default App;
